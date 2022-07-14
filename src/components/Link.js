bb719abe9bdcc428e40fc3d5ef775dc7a7dea7b9
Jean <chakib3500@gmail.com>
2022-07-14 22:38:04 +0200

    link hover

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Link({text,target,icon}) 
 {
-    return <a className='flex gap-1 mx-3' href={`${target}`}>
+    return <a className='flex gap-1 mx-3 link-hover' href={`${target}`}>
             {icon && <img className='w-5' src={`${icon}`}/>}
             {text}
         </a>
